069f28ec90604913295a29becabf9b54a7c9334a
projectmandiri10-lang <projectmandiri10@gmail.com>
2026-01-06 23:38:40 +0700

    fix: disable static export to resolve Vercel build path error

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  // output: 'export',
   // Images unoptimized is required for static export if using next/image
-  images: {
-    unoptimized: true,
-  },
+  // images: {
+  //   unoptimized: true,
+  // },
 }
 
 module.exports = nextConfig
